c48fe4c365e8fba5f0eae72a1a57f6a495f5615f
Michiel Jansen <11848435+michielmj@users.noreply.github.com>
2026-02-16 07:58:45 +0100

    WIP: my-model implementation

--- sample/my-model/src/my_model_pkg/orm.py
+++ sample/my-model/src/my_model_pkg/orm.py
@@ -12,22 +12,7 @@ stockingpoint_data = Table(
     Column("ip_target", Float, nullable=False),
     Column("mean_demand", Float, nullable=True),
     Column("std_demand", Float, nullable=True),
-    # ...
 )
-
-supplier_data = Table(
-    "supplier_data",
-    metadata,
-    Column("scenario_id", String, nullable=False),
-    Column("key", String, nullable=False),
-    Column("echelon", Integer, nullable=False),
-    Column("location", String, nullable=False),
-    Column("ip_target", Float, nullable=False),
-    Column("mean_demand", Float, nullable=True),
-    Column("std_demand", Float, nullable=True),
-    # ...
-)
-
 
 demand_data = Table(
     "demand_data",
@@ -35,7 +20,7 @@ demand_data = Table(
     Column("scenario_id", String, nullable=False),
     Column("source_key", String, nullable=False),
     Column("target_key", String, nullable=False),
-    # ...
+    Column("lead_time", String, nullable=False),
 )
 
 supply_data = Table(
--- sample/my-model/src/my_model_pkg/sampling.py
+++ sample/my-model/src/my_model_pkg/sampling.py
@@ -1,19 +1,23 @@
 import numpy as np
+import pandas as pd
+from graphblas import Vector, op
+from toolbox import distributions
+
 
 def sample_dists(
-    random_state: np.random.RandomState,
-    dists: list[tuple[any, np.ndarray, np.ndarray]],
-    sample_indices: np.ndarray,
-    round: bool = False,
-    lower: float = -float("inf"),
-    upper: float = float("inf"),
+        random_state: np.random.RandomState,
+        dists: list[tuple[any, np.ndarray, np.ndarray]],
+        sample_indices: np.ndarray,
+        round_values: bool = False,
+        lower: float = -float("inf"),
+        upper: float = float("inf"),
 ) -> Vector:
-    sample = Vector.zeros(sample_indices)
+    sample = Vector.from_coo(indices=sample_indices, values=np.zeros_like(sample_indices, dtype=np.dtypes.Float64DType))
 
     for dist, indices, params in dists:
-        needed = Array.ind_isin(indices, sample_indices)
-        needed_indices = indices[needed].copy()
-        if needed_indices.shape[0] != 0:
+
+        needed = np.intersect1d(indices, sample_indices)
+        if needed.shape[0] != 0:
             needed_params = params[:, needed]
 
             values = dist.rvs(*needed_params, random_state=random_state)
@@ -21,36 +25,37 @@ def sample_dists(
             if values.ndim == 0:
                 values = np.asarray([values])
 
-            if round:
+            if round_values:
                 values = np.round(values)
 
             values[values < lower] = lower
             values[values > upper] = upper
 
-            sample += Vector(indices=needed_indices, values=values)
+            sample(op.plus) << Vector.from_coo(indices=needed, values=values)
 
     return sample
 
 
 def get_dists(data: pd.DataFrame, dists_column: str, params_column: str):
     dists = []
-    for dist_name in data[dists_column].unique():
-        if dist_name is not None:
-            try:
-                dist = getattr(scipy_rv, dist_name)
-            except:
-                raise ValueError(f"Unsupported distribution type: {dist_name}.")
-            else:
-                df = data.loc[data[dists_column] == dist_name, [INDEX, params_column]]
-                indices = df[INDEX].to_numpy(dtype=np.uint32)
-                params = np.vstack(df[params_column]).T
-                non_positive_means = dist.mean(*params) <= 0.0
-                if np.any(non_positive_means):
-                    raise SimulationRuntimeError(
-                        f"There are distributions with non-positive means (e.g. with indices: "
-                        f"{str(indices[non_positive_means][:10])})."
-                    )
 
-                dists.append((dist, indices, params))
+    data = data.dropna(subset=[dists_column])
+    for dist_name in data[dists_column].unique():
+        try:
+            dist = getattr(distributions, dist_name)
+        except:
+            raise ValueError(f"Unsupported distribution type: {dist_name}.")
+        else:
+            df = data.loc[data[dists_column] == dist_name, [params_column]]
+            indices = df.index.to_numpy(dtype=np.int64)
+            params = np.vstack(df[params_column]).T
+            non_positive_means = dist.mean(*params) <= 0.0
+            if np.any(non_positive_means):
+                raise ValueError(
+                    f"There are distributions with non-positive means (e.g. with indices: "
+                    f"{str(indices[non_positive_means][:10])})."
+                )
+
+            dists.append((dist, indices, params))
 
     return dists
